a261ae3b96f6be173094397b1202481df9bcb871
Hemily Batista <hemilyoliveira02@gmail.com>
2024-11-11 10:52:28 -0300

    leetcode

--- 002.js
+++ 002.js
@@ -90,3 +90,61 @@ const compressNames = (names) => {
 }
 
 // console.log(compressNames(["John", "John", "Mary", "Mary", "Mary", "David"]))
+
+// -------------------------------------------------------------
+
+const charInGroups = (chars, t) => {
+  let newArr = chars.split('')
+  let arr = []
+
+  for (let i = 0; i < newArr.length; i += t) {
+    let gp = newArr.slice(i, i + t);
+    arr.push(...gp.reverse())
+  }
+  return arr.join('')
+}
+
+// console.log(charInGroups("abcdefg", 4))
+
+// -------------------------------------------------------------
+
+const removeDuplicateChar = (str) => {
+  let arr = []
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      arr.push(str[i])
+    }
+  }
+  return arr.join('')
+}
+
+// console.log(removeDuplicateChar("aabbccdd"))
+
+// -------------------------------------------------------------
+
+const replaceSpaces = (str) => {
+  return str.replace(/\s/g, "%20");
+}
+
+// console.log(replaceSpaces("Hello World! How are you?"))
+
+// -------------------------------------------------------------
+
+const countWords = (str) => {
+  let st = str.split(' ')
+  return st.length
+}
+
+// console.log(countWords("Hello, how are you doing?"))
+
+// -------------------------------------------------------------
+
+const strPalindrome = (str) => {
+  let str1 = str.split('').reverse().join('')
+  if (str1 === str) {
+    return true
+  }
+  return false
+}
+
+// console.log(strPalindrome("racecar"))
